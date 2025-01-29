7280ea594967e1fc9e2ad6ce40363b2a5af200af
mfarazzz <muhammadfaraz1547@gmail.com>
2025-01-29 21:06:08 +0530

    Implementation: add() supports custom delimiters

--- lib/string_calculator_tdd.dart
+++ lib/string_calculator_tdd.dart
@@ -1,7 +1,15 @@
 int add(String numbers) {
   if (numbers.isEmpty) return 0;
+
+  String delimiter = r'[,\n]';
+  if (numbers.startsWith("//")) {
+    var parts = numbers.split("\n");
+    delimiter = RegExp.escape(parts[0].substring(2));
+    numbers = parts[1];
+  }
+
   return numbers
-      .split(RegExp(r'[,\n]')) // Handle both comma and newline
+      .split(RegExp(delimiter))
       .map(int.parse)
       .reduce((a, b) => a + b);
 }
--- test/string_calculator_tdd_test.dart
+++ test/string_calculator_tdd_test.dart
@@ -2,7 +2,7 @@ import 'package:string_calculator_tdd/string_calculator_tdd.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('handles newlines as a delimiter', () {
-    expect(add("1\n2,3"), equals(6));
+  test('supports custom delimiter', () {
+    expect(add("//;\n1;2"), equals(3));
   });
 }
