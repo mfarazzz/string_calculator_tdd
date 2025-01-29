6be3bc168f39667eb674073064b6f3ca9d93f715
mfarazzz <muhammadfaraz1547@gmail.com>
2025-01-29 20:21:16 +0530

    Implementation: add() handles a single number

--- lib/string_calculator_tdd.dart
+++ lib/string_calculator_tdd.dart
@@ -1,4 +1,4 @@
 int add(String numbers) {
   if (numbers.isEmpty) return 0;
-  return 0;
+  return int.parse(numbers);
 }
--- test/string_calculator_tdd_test.dart
+++ test/string_calculator_tdd_test.dart
@@ -2,7 +2,7 @@ import 'package:string_calculator_tdd/string_calculator_tdd.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('returns 0 for an empty string', () {
-    expect(add(""), equals(0));
+  test('returns number itself for a single number', () {
+    expect(add("1"), equals(1));
   });
 }
