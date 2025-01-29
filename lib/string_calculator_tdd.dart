0689aad690adef09c0fff4fd09b931f4cc46904f
mfarazzz <muhammadfaraz1547@gmail.com>
2025-01-29 20:42:07 +0530

    Implementation: add() supports newline delimiters

--- lib/string_calculator_tdd.dart
+++ lib/string_calculator_tdd.dart
@@ -1,4 +1,7 @@
 int add(String numbers) {
   if (numbers.isEmpty) return 0;
-  return numbers.split(',').map(int.parse).reduce((a, b) => a + b);
+  return numbers
+      .split(RegExp(r'[,\n]')) // Handle both comma and newline
+      .map(int.parse)
+      .reduce((a, b) => a + b);
 }
--- test/string_calculator_tdd_test.dart
+++ test/string_calculator_tdd_test.dart
@@ -2,7 +2,7 @@ import 'package:string_calculator_tdd/string_calculator_tdd.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('handles multiple numbers', () {
-    expect(add("1,2,3,4,5"), equals(15));
+  test('handles newlines as a delimiter', () {
+    expect(add("1\n2,3"), equals(6));
   });
 }
