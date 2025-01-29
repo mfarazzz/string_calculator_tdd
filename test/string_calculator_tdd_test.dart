f281714fd5cbd9b047ef7e3e231cd718a5edbe67
mfarazzz <muhammadfaraz1547@gmail.com>
2025-01-29 20:32:14 +0530

    Implementation: add() handles multiple numbers

--- test/string_calculator_tdd_test.dart
+++ test/string_calculator_tdd_test.dart
@@ -2,7 +2,7 @@ import 'package:string_calculator_tdd/string_calculator_tdd.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('returns sum of two numbers', () {
-    expect(add("1,2"), equals(3));
+  test('handles multiple numbers', () {
+    expect(add("1,2,3,4,5"), equals(15));
   });
 }
